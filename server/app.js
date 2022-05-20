39bc85506219237db1f8f38ef53f74514972e972
Ange <81535114+angelacollings@users.noreply.github.com>
2022-05-20 12:59:31 +1200

    use content router

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const contentRouter = require("./routes/contentRoutes");
+
 //middleware
 app.use(cors());
 app.use(express.json());
 
 //routes
-// app.use("/api/content");
+app.use("/api/content", contentRouter);
 
 module.exports = app;
