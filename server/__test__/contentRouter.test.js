bde40e52db1c546b500c70197b77ac1b45c346e1
Angela Collings <angelacollings@developersinstitute.co.nz>
2022-05-30 12:47:19 +1200

    setup for test, write tests

---
+++ server/__test__/contentRouter.test.js
@@ -0,0 +1,73 @@
+const { Pool } = require("pg");
+const request = require("supertest");
+
+const app = require("../app");
+
+const content = [
+    {
+      id: 1,
+      description:
+        "Some content"
+    },
+    {
+      id: 2,
+      description: "content"
+    },
+  ];
+
+// const content = require("../mocks/data");
+
+// setup to mock pg
+jest.mock("pg", () => {
+  const mPool = {
+    query: jest.fn(),
+  };
+  return { Pool: jest.fn(() => mPool) };
+});
+
+let pool;
+// before each test case
+beforeEach(() => {
+  pool = new Pool();
+});
+// clean up after each test case done
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('Given we have an "/api/content" endpoint', () => {
+  // GET Routes
+  it("When a valid GET request is made, then a 200 status code is returned with an array of questions", async () => {
+    pool.query.mockResolvedValue({ rows: content });
+    const checkBody = (res) => {
+      expect(res.body).toStrictEqual(content);
+      expect(pool.query).toBeCalledTimes(1);
+      expect(pool.query).toHaveBeenCalledWith("SELECT * FROM content;");
+    };
+
+    await request(app)
+      .get("/api/content")
+      .set("Content-Type", "application/json")
+      .expect("Content-Type", /application\/json/)
+      .expect(checkBody)
+      .expect(200);
+  });
+
+  it("When a valid GET request is made and there is a database error, then a 500 status code is returned", async () => {
+    pool.query.mockRejectedValue(new Error("failed connection"));
+    const checkBody = (res) => {
+      expect(res.body).toStrictEqual({
+        message: `Database query failed: failed connection`,
+      });
+      expect(pool.query).toBeCalledTimes(1);
+      expect(pool.query).toHaveBeenCalledWith("SELECT * FROM content;");
+    };
+
+    await request(app)
+      .get("/api/content")
+      .set("Content-Type", "application/json")
+      .expect("Content-Type", /application\/json/)
+      .expect(checkBody)
+      .expect(500);
+  });
+});
